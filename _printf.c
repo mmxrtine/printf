253bf6c49d19d2df514ec7ee1bde939e151e5452
mmxrtine <michellediawara@gmail.com>
2023-11-14 05:56:49 -0800

    task 0

--- _printf.c
+++ _printf.c
@@ -2,64 +2,60 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/**
-* _printf - function to print
-* format: first arguement
-* return: number of characters printed
-*/
-
 int _printf(const char *format, ...)
 {
-	int char_print = 0;
-	va_list args;
-	
-	if (format == NULL)
-	{
-		return (-1);
-	}
-	va_start (args, format);
+    int char_print = 0;
+    va_list args;
 
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			char_print++;
-		}
-		else /* if format is the % sign */
-		{
-			format++; /* skip & sign */
-			if (*format == '\0')
-			{	 
-				break;
-			}
-			if (*format == '%')
-			{
-				write(1, format, 1); 
-				char_print++;
-			}
-			else if (*format == 'c')
-			{
-				char c = (char)va_arg (args, int);
-				write(1, &c, 1);
-				char_print++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(args, char*);
-				int str_len = 0;
+    if (format == NULL)
+    {
+        return (-1);
+    }
 
-				while (str[str_len] != '\0')
-					str_len++;
-	
-				write(1, str, str_len);
-				char_print += str_len;
-			}
-		}
-		
-		format++;
-	}
-	
-	va_end(args);
-	return (char_print);	
+    va_start(args, format);
+
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            char_print++;
+        }
+        else
+        {
+            format++;
+            if (*format == '\0')
+            {
+                break;
+            }
+            if (*format == '%')
+            {
+                write(1, format, 1);
+                char_print++;
+            }
+            else if (*format == 'c')
+            {
+                char c = (char)va_arg(args, int); // Cast to char
+                write(1, &c, 1);
+                char_print++;
+            }
+            else if (*format == 's')
+            {
+                char *str = va_arg(args, char *);
+                int str_len = 0;
+
+                while (str[str_len] != '\0')
+                    str_len++;
+
+                write(1, str, str_len);
+                char_print += str_len;
+            }
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return (char_print);
 }
+
